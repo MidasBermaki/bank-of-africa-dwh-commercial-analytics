b9651ce31c31e90732a0bc4155871e1c5268f214
Aymene Bermaki <aymene.bermaki@gmail.com>
2025-08-18 12:37:39 +0100

    Create proc_load_bronze_erp_tables.sql

---
+++ scripts/bronze/proc_load_bronze/proc_load_bronze_erp_tables.sql
@@ -0,0 +1,183 @@
+/*
+=========================================================================================================================
+Procédure stockée : Chargement de la couche Bronze (Source -> Bronze)
+=========================================================================================================================
+Objectif du script :
+    Cette procédure stockée charge les données dans le schéma 'bronze' à partir de fichiers CSV externes.
+    Elle effectue les actions suivantes :
+    - Tronque les tables du schéma bronze avant de charger les données.
+    - Utilise la commande `BULK INSERT` pour importer les données des fichiers CSV vers les tablebronze.e.
+
+Paramétres :
+    Aucun.
+    Cette procédure stockée n'accepte aucun paramétre et ne retourne aucune valeur.
+
+Exemple d'utilisation :
+    EXEC bronze.load_bronze;
+=========================================================================================================================
+=========================================================================================================================
+Stored Procedure: Loading the Bronze Layer (Source -> Bronze)
+=========================================================================================================================
+Script Objective:
+    This stored procedure loads data into the 'bronze' schema from external CSV files.
+    It performs the following actions:
+    - Truncates the tables in the bronze schema before loading the data.
+    - Uses the `BULK INSERT` command to import data from the CSV files into the bronze tables.
+
+Parameters:
+    None.
+    This stored procedure does not accept any parameters and does not return any value.
+
+Example usage:
+    EXEC bronze.load_bronze;
+=========================================================================================================================
+*/
+
+CREATE OR ALTER PROCEDURE bronze.load_bronze_erp_tables AS
+BEGIN
+	DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @batch_end_time DATETIME; 
+	BEGIN TRY
+		SET @batch_start_time = GETDATE();
+		PRINT '================================================';
+		PRINT 'Loading Bronze Layer ERP Tables';
+		PRINT '================================================';
+		-------------------------------------------------------------------------------------
+		--- Agences -------------------------------------------------------------------------
+		-------------------------------------------------------------------------------------
+		SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: bronze.erp_agence_info';
+		TRUNCATE TABLE bronze.erp_agence_info;
+		PRINT '>> Inserting Data Into: bronze.erp_agence_info';
+		BULK INSERT bronze.erp_agence_info
+		FROM 'C:\Users\X 250\Downloads\Stage Bank of Africa\Project Bank of Africa\Data\source_ERP\agences.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '>> -------------';
+		
+		-------------------------------------------------------------------------------------
+		--- Entreprise ----------------------------------------------------------------------
+		-------------------------------------------------------------------------------------
+		SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: bronze.erp_entrepise_info';
+		TRUNCATE TABLE bronze.erp_entreprise_info;
+		PRINT '>> Inserting Data Into: bronze.erp_entrepise_info';
+		BULK INSERT bronze.erp_entreprise_info
+		FROM 'C:\Users\X 250\Downloads\Stage Bank of Africa\Project Bank of Africa\Data\source_ERP\entreprises2.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ';',
+			CODEPAGE = '65001',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '>> -------------';
+		-------------------------------------------------------------------------------------
+		--- Historique Evenements -----------------------------------------------------------
+		-------------------------------------------------------------------------------------
+		SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: bronze.erp_historique_evenements';
+		TRUNCATE TABLE bronze.erp_historique_evenements;
+		PRINT '>> Inserting Data Into: bronze.erp_historique_evenements';
+		BULK INSERT bronze.erp_historique_evenements
+		FROM 'C:\Users\X 250\Downloads\Stage Bank of Africa\Project Bank of Africa\Data\source_ERP\historique_evenements.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '>> -------------';
+
+		-------------------------------------------------------------------------------------
+		--- Famille Crédits d'Investissement Info -------------------------------------------
+		-------------------------------------------------------------------------------------
+		SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: bronze.erp_produit_cmlt_info';
+		TRUNCATE TABLE bronze.erp_produit_cmlt_info;
+		PRINT '>> Inserting Data Into: bronze.erp_produit_cmlt_info';
+		BULK INSERT bronze.erp_produit_cmlt_info
+		FROM 'C:\Users\X 250\Downloads\Stage Bank of Africa\Project Bank of Africa\Data\source_ERP\produits_CMLT.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '>> -------------';
+		-------------------------------------------------------------------------------------
+		--- Famille Excédents de Trésorerie Info --------------------------------------------
+		-------------------------------------------------------------------------------------
+		SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: bronze.erp_produit_depot_info';
+		TRUNCATE TABLE bronze.erp_produit_depot_info;
+		PRINT '>> Inserting Data Into: bronze.erp_produit_depot_info';
+		BULK INSERT bronze.erp_produit_depot_info
+		FROM 'C:\Users\X 250\Downloads\Stage Bank of Africa\Project Bank of Africa\Data\source_ERP\produits_Depot.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '>> -------------';
+		-------------------------------------------------------------------------------------
+		--- Famille Ligne de Fonctionnement Info --------------------------------------------
+		-------------------------------------------------------------------------------------
+		SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: bronze.erp_produit_ldf_info';
+		TRUNCATE TABLE bronze.erp_produit_ldf_info;
+		PRINT '>> Inserting Data Into: bronze.erp_produit_ldf_info';
+		BULK INSERT bronze.erp_produit_ldf_info
+		FROM 'C:\Users\X 250\Downloads\Stage Bank of Africa\Project Bank of Africa\Data\source_ERP\produits_LDF.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '>> -------------';
+				-------------------------------------------------------------------------------------
+		--- Metriques Mensuelles ------------------------------------------------------------
+		-------------------------------------------------------------------------------------
+		SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: bronze.erp_metriques';
+		TRUNCATE TABLE bronze.erp_metriques;
+		PRINT '>> Inserting Data Into: bronze.erp_metriques';
+		BULK INSERT bronze.erp_metriques
+		FROM 'C:\Users\X 250\Downloads\Stage Bank of Africa\Project Bank of Africa\Data\source_ERP\metriques_mensuelles.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '>> -------------';
+
+		
+		SET @batch_end_time = GETDATE();
+		PRINT '=========================================='
+		PRINT 'Loading Bronze Layer ERP Tables is Completed';
+        PRINT '   - Total Load Duration: ' + CAST(DATEDIFF(SECOND, @batch_start_time, @batch_end_time) AS NVARCHAR) + ' seconds';
+		PRINT '=========================================='
+	END TRY
+	BEGIN CATCH
+		PRINT '=========================================='
+		PRINT 'ERROR OCCURED DURING LOADING BRONZE LAYER'
+		PRINT 'Error Message' + ERROR_MESSAGE();
+		PRINT 'Error Message' + CAST (ERROR_NUMBER() AS NVARCHAR);
+		PRINT 'Error Message' + CAST (ERROR_STATE() AS NVARCHAR);
+		PRINT '=========================================='
+	END CATCH
+END
+
